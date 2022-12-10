d6f5315928336ee3b611797c83f8e86d6b44959a
crystal025 <oj3339@naver.com>
2022-12-10 12:04:04 +0900

    #46 마우스 커서 이미지 크기 수정

--- mini-projects/merry/main.js
+++ mini-projects/merry/main.js
@@ -23,10 +23,12 @@ start.addEventListener("click", () => {
           started = false;
           time = 20;
           alert(`훌륭하시군요! 점수는${score}점!`);
+          score = 0;
         } else {
           started = false;
           time = 20;
           alert(`아쉽군요! 점수는${score}점! 다시 도전해 보세요!`);
+          score = 0;
         }
       }, 50);
     }
